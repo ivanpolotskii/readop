30a7ba7073459437d965905e8c1a6495ba32a74e
mentalist01 <58345789+mentalist01@users.noreply.github.com>
2020-08-08 18:24:32 +0300

    chapters list added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 let chapterNumber = document.querySelector('.chapter-number');
 let continues = document.querySelector('.continue > h1');
-if(localStorage.getItem('ch')){
+if (localStorage.getItem('ch')) {
 	chapterNumber.textContent = `One Piece Chapter ${localStorage.getItem('ch')}`;
-}else{
+} else {
 	continues.textContent = 'Start Reading';
 	chapterNumber.textContent = 'Start with first chapter';
+}
+// Add chapters
+let count = 8;
+let chaptersContainer = document.querySelector('.last')
+while (count-- > 1) {
+	chaptersContainer.insertAdjacentHTML('beforeend', `
+		<div class = "chapter-item" id = "${count}" onclick = "localStorage.setItem('ch',this.id+'');document.location.href='./onepiece.html'">
+			<div class = "description"> One Piece Chapter ${count}</div>
+			<div class = "buttons">
+				<a href = "./onepiece.html">READ</a>
+			</div>
+		</div>
+	`)
 }
